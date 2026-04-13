5b0efd2e0e150f8a737f7671e10a3af24fd54ca0
MateuszKasprzak2tc <mateusz.kasprzak@tm1.edu.pl>
2026-04-13 10:26:02 +0200

    Added a form component to create new tickets and updated the ticket card to display the new ticket information. Also updated the package.json to include necessary dependencies for the form component.

--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -1,7 +1,7 @@
 function TicketCard({ticket}){
     <div className="card">
         <h3>{ticket.klient}</h3>
-        <p>Urządzenie: {klient.urządzenie}</p>
+        <p>Urządzenie: {klient.urzadzenie}</p>
         <p>usterka: {klient.usterka}</p>
         <p>status: {klient.status}</p>
         <p>priorytet: {klient.priorytet}</p>
